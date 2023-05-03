4fb8437d84792ff469109160f824246316e19277
alextitonis <alexandrostitonis@gmail.com>
2023-05-03 15:30:12 +0300

    langchain by bruvh

--- gpt.py
+++ gpt.py
@@ -10,13 +10,17 @@ Output:
 """
 
 def getMemoryQueries(memories):
-    memories = [x["memory"] for x in memories]
+    memories = [x for x in memories]
     memories = "\n".join([str(i+1)+". "+x for i,x in enumerate(memories)])
 
     bot = ChatBot("")
 
     prompt = queriesPrompt.format(memories)
+    #print('ChatGPT prompt:', prompt)
+
     queries = bot(prompt)
+    #print('ChatGPT queries:', queries)
+
     queries = queries.split("\n")
     queries = [x[3:] for x in queries]
     return queries
